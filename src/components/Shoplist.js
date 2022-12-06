757fccfdc231173ad647d07711e3f7d8bc83db44
MarzioCostantini <costantinimarzio@gmail.com>
2022-12-06 08:42:57 +0100

    fetch done

--- src/components/Shoplist.js
+++ src/components/Shoplist.js
@@ -1,4 +1,14 @@
+import { useState, useEffect } from "react";
+
 const Shoplist = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+  console.log(data);
   return <section></section>;
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
+import Shoplist from "../components/Shoplist";
+
 const Home = () => {
   return (
     <div>
       <h1>Hi Steffen</h1>
+      <Shoplist />
     </div>
   );
 };
